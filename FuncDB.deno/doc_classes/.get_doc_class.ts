cd9aeefcab831f800f80710e0519a478e785332f
balajahe <56229110+balajahe@users.noreply.github.com>
2020-01-20 07:23:13 +0300

    Create .get_doc_class.ts

---
+++ FuncDB.deno/doc_classes/.get_doc_class.ts
@@ -0,0 +1,12 @@
+import { DocClass } from '../core/DBMeta.ts'
+
+import ref from './ref.ts'
+import purch from './purch.ts'
+
+export default function get_doc_class(classname: string) {
+    switch (classname) {
+        case 'ref': return ref
+        case 'purch': return purch
+        default: return DocClass
+    }
+}
